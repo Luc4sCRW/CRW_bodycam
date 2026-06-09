985e90f1155c8f0c9b40e5f7bc87198547e24c89
Luc4s_CRW <luc4sbuisnesscrw@gmail.com>
2026-06-10 00:20:16 +0200

    Update de.lua

--- locales/de.lua
+++ locales/de.lua
@@ -1,4 +1,5 @@
 Locales['de'] = {
     ['dialog_activated'] = 'Bodycam wird angelegt...',
-    ['dialog_deactivated'] = 'Bodycam wird abgelegt...'
+    ['dialog_deactivated'] = 'Bodycam wird abgelegt...',
+    ['dialog_permission'] = 'Du hast dafür keine Berechtigung!'
 }
