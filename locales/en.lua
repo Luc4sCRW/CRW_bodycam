09e48622fd23204c5db6658c611e3682362e5fc6
Luc4s_CRW <luc4sbuisnesscrw@gmail.com>
2026-06-10 00:22:03 +0200

    Create en.lua

---
+++ locales/en.lua
@@ -0,0 +1,5 @@
+Locales['de'] = {
+    ['dialog_activated'] = 'Bodycam is being put on...',
+    ['dialog_deactivated'] = 'Bodycam is being removed...',
+    ['dialog_permission'] = 'You do not have permission to do that!'
+}
