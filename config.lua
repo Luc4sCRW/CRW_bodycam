6108d4ab8a4ed74606e2b4fd5881f36eab26a78c
Luc4s_CRW <luc4sbuisnesscrw@gmail.com>
2026-06-09 15:09:58 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -15,3 +15,25 @@ Config.AllowedJobs = {
 
 -- Logo (Entweder ein Link oder lokal im html Ordner z.B. "logo.png")
 Config.LogoPath = "https://static.wikia.nocookie.net/gtawiki/images/3/3f/Coil-GTAO-Logo.png"
+
+
+Config.Locale = 'en'
+
+Locales = {}
+function i18n(str, ...)
+    local args = {...}
+    if not str then return 'Key nil' end
+    local localeCfg = Locales[Config.Locale]
+    local translation = localeCfg and localeCfg[str] or (Locales['en'] and Locales['en'][str])
+    if translation then
+        local _, count = translation:gsub("%%s", "")
+        if count > 0 and #args == 0 then
+            return translation
+        end
+        return string.format(translation, ...)
+    end
+    return 'Translation [' .. Config.Locale .. '][' .. str .. '] does not exist'
+end
+function _U(str, ...)
+    return i18n(str, ...)
+end
