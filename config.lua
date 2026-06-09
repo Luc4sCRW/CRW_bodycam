1115c38b9f1f1e801eb3f3c0fc5b9fb474e2b1f9
Luc4s_CRW <luc4sbuisnesscrw@gmail.com>
2026-06-09 14:32:29 +0200

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,17 @@
+Config = {}
+
+Config.ItemName = "bodycam" -- OX_INV
+Config.BodycamTitle = "COIL BODYCAM"
+Config.DateFormat = "ISO" -- EU (DD.MM.YYYY), US (MM/DD/YYYY), ISO (YYYY-MM-DD)
+Config.IncludeSeconds = true -- true/false
+
+-- Erlaubte Jobs
+Config.AllowedJobs = {
+    ["police"] = true,
+    ["ambulance"] = true,
+    ["lsfd"] = true,
+    ["noose"] = true,
+}
+
+-- Logo (Entweder ein Link oder lokal im html Ordner z.B. "logo.png")
+Config.LogoPath = "https://static.wikia.nocookie.net/gtawiki/images/3/3f/Coil-GTAO-Logo.png"
