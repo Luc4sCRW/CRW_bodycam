d691fb1fab4e7982ab33fb17b8290aafa7e761fd
Luc4s_CRW <luc4sbuisnesscrw@gmail.com>
2026-06-11 16:53:17 +0200

    Update server.lua
    .

--- server.lua
+++ server.lua
@@ -7,7 +7,7 @@ exports('useBodycam', function(event, item, inventory, slot, data)
         
         if not xPlayer then return false end
 
-        -- Job Prüfung
+        -- Job Check
         if Config.AllowedJobs[xPlayer.job.name] then
             local name = xPlayer.getName()
             TriggerClientEvent('CRW_bodycam:toggle', source, name)
@@ -15,18 +15,18 @@ exports('useBodycam', function(event, item, inventory, slot, data)
         else
             TriggerClientEvent('ox_lib:notify', source, {
                 type = 'error', 
-                description = 'Du hast keine Berechtigung für dieses Gerät.'
+                description = _U('dialog_permission')
             })
             return false
         end
     end
 end)
 
--- Callback für den Item-Check
+-- Callback für den Item Check // Callback for the item check
 ESX.RegisterServerCallback('CRW_bodycam:checkItem', function(source, cb)
     local xPlayer = ESX.GetPlayerFromId(source)
     if not xPlayer then return cb(false) end
-    
+        
     local item = xPlayer.getInventoryItem(Config.ItemName)
     if item and item.count > 0 then
         cb(true)
