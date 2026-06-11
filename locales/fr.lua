90d747a390112a65b5d91d49f129e4372ca65118
Luc4s_CRW <luc4sbuisnesscrw@gmail.com>
2026-06-11 02:19:51 +0200

    fr.lua erstellen

---
+++ locales/fr.lua
@@ -0,0 +1,5 @@
+Locales['fr'] = {
+    ['dialog_activated'] = 'Mise en place de la caméra-piéton...',
+    ['dialog_deactivated'] = 'Retrait de la caméra-piéton...',
+    ['dialog_permission'] = 'Vous n’avez pas l’autorisation pour cela !'
+}
