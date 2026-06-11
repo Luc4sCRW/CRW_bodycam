83a8bab6e3eabcc8fc4746e31b63bb454dc2ed3d
Luc4s_CRW <luc4sbuisnesscrw@gmail.com>
2026-06-11 02:19:14 +0200

    es.lua erstellen

---
+++ locales/es.lua
@@ -0,0 +1,5 @@
+Locales['es'] = {
+    ['dialog_activated'] = 'Colocando la cámara corporal...',
+    ['dialog_deactivated'] = 'Quitando la cámara corporal...',
+    ['dialog_permission'] = '¡No tienes permiso para hacer esto!'
+}
