357f5f819786809d1d6b5156a10514102d533fd7
Luc4s_CRW <luc4sbuisnesscrw@gmail.com>
2026-06-11 17:16:19 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,17 +1,17 @@
 Config = {}
 
 Config.Locale = 'en' -- 'de', 'en', 'es', 'fr'
-Config.ItemName = "bodycam" -- ox_inventory Item
-Config.BodycamTitle = "COIL BODYCAM"
-Config.DateFormat = "ISO" -- EU (DD.MM.YYYY), US (MM/DD/YYYY), ISO (YYYY-MM-DD)
+Config.ItemName = 'bodycam' -- ox_inventory Item
+Config.BodycamTitle = 'COIL BODYCAM'
+Config.DateFormat = 'ISO' -- EU (DD.MM.YYYY), US (MM/DD/YYYY), ISO (YYYY-MM-DD)
 Config.IncludeSeconds = true
 
 Config.AllowedJobs = {
-    ["police"] = true,
-    ["ambulance"] = true,
-    ["lsfd"] = true,
-    ["noose"] = true,
+    ['police'] = true,
+    ['ambulance'] = true,
+    ['lsfd'] = true,
+    ['noose'] = true,
 }
 
 -- Logo
-Config.LogoPath = "https://static.wikia.nocookie.net/gtawiki/images/3/3f/Coil-GTAO-Logo.png" -- if you want to use a picture local, then html/YOUR-IMAGE.png
+Config.LogoPath = 'https://static.wikia.nocookie.net/gtawiki/images/3/3f/Coil-GTAO-Logo.png' -- if you want to use a picture local, then html/YOUR-IMAGE.png
